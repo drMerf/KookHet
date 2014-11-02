c76a9788be91298bce77e5518439b7de69ab6358
Bert <maerten.bert@outlook.com>
2014-11-02 03:10:08 +0100

    X

--- KookHet/app/src/main/java/be/howest/nmct/android/kookhet/dummy/DummyContent.java
+++ KookHet/app/src/main/java/be/howest/nmct/android/kookhet/dummy/DummyContent.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// Helper class for providing sample content for user interfaces created by Android template wizards.
+import javax.xml.datatype.Duration;
+
 // TODO: Replace all uses of this class before publishing your app.
+// Helper class for providing sample content for user interfaces created by Android template wizards.
 public class DummyContent {
 
     // An array of sample items.
@@ -17,54 +19,62 @@ public class DummyContent {
     public static Map<String, Categorie> CATEGORIEEN_MAP = new HashMap<String, Categorie>();
     public static Map<String, Recept> RECEPTEN_MAP = new HashMap<String, Recept>();
 
-    static {
-        // Add sample items.
-        addItem(new Categorie("1", "Categorie 1"));
-        addItem(new Categorie("2", "Categorie 2"));
-        addItem(new Categorie("3", "Categorie 3"));
-        addItem(new Recept("1", "Recept 1"));
-        addItem(new Recept("2", "Recept 2"));
-        addItem(new Recept("3", "Recept 3"));
-        addItem(new Recept("4", "Recept 4"));
-    }
-
-    public static class Categorie {
-        public String id;
-        public String content;
-
-        public Categorie(String id, String content) {
-            this.id = id;
-            this.content = content;
-        }
-
-        @Override
-        public String toString() {
-            return content;
-        }
-    }
-
     private static void addItem(Categorie item) {
         CATEGORIEEN.add(item);
         CATEGORIEEN_MAP.put(item.id, item);
     }
 
-    public static class Recept {
-        public String id;
-        public String content;
+    private static void addItem(Recept item) {
+        RECEPTEN.add(item);
+        RECEPTEN_MAP.put(item.id, item);
+    }
 
-        public Recept(String id, String content) {
+    static {
+        addItem(new Categorie("1", "Voorgerechten"));
+        addItem(new Categorie("2", "Hoofdgerechten"));
+        addItem(new Categorie("3", "Deserts"));
+        addItem(new Recept("1", "1", "Tomatensoep met balletjes", "", null, false));
+        addItem(new Recept("2", "2", "Spaghetti Bolognese", "", null, false));
+        addItem(new Recept("3", "2", "Kip met appelmoes en kroketjes", "", null, false));
+        addItem(new Recept("4", "2", "Stoofvlees met frieten", "", null, false));
+        addItem(new Recept("5", "3", "Chocolademousse", "", null, false));
+    }
+
+    public static class Categorie {
+        public String id;
+        public String naam;
+
+        public Categorie(String id, String naam) {
             this.id = id;
-            this.content = content;
+            this.naam = naam;
         }
 
         @Override
         public String toString() {
-            return content;
+            return naam;
         }
     }
 
-    private static void addItem(Recept item) {
-        RECEPTEN.add(item);
-        RECEPTEN_MAP.put(item.id, item);
+    public static class Recept {
+        public String id;
+        public String naam;
+        public String categorie;
+        public String bereidingswijze;
+        public Duration bereidingstijd;
+        public boolean vegetarisch;
+
+        public Recept(String id, String categorie, String naam, String bereidingswijze, Duration bereidingstijd, boolean vegetarisch) {
+            this.id = id;
+            this.naam = naam;
+            this.categorie = categorie;
+            this.bereidingswijze = bereidingswijze;
+            this.bereidingstijd = bereidingstijd;
+            this.vegetarisch = vegetarisch;
+        }
+
+        @Override
+        public String toString() {
+            return naam;
+        }
     }
 }
